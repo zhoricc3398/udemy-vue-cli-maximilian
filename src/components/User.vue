60375f8394020a47d6fb6bc62da95ecf645231c2
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-02 11:26:32 +0400

    feat(source): 114. Communication between Sibling Components

--- src/components/User.vue
+++ src/components/User.vue
@@ -7,10 +7,15 @@
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <app-user-detail :myName="name" @nameWasReset="name = $event" :resetFn="resetName"></app-user-detail>
+        <app-user-detail
+          :myName="name"
+          @nameWasReset="name = $event"
+          :resetFn="resetName"
+          :userAge="age"
+        ></app-user-detail>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
+        <app-user-edit :userAge="age" @ageWasEdited="age = $event"></app-user-edit>
       </div>
     </div>
   </div>
@@ -23,7 +28,8 @@ import UserEdit from "./UserEdit.vue";
 export default {
   data() {
     return {
-      name: "Max"
+      name: "Max",
+      age: 27
     };
   },
   methods: {
