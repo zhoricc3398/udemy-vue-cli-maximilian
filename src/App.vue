54720d6c6e5d691ce215ff9daf4091d4632ca6c8
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-02 15:01:57 +0400

    feat(source): 123. Passing Content with Slots

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,22 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-user></app-user>
+        <app-quote>
+          <h2>The Quote</h2>
+          <p>A wonderful Quote</p>
+        </app-quote>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import User from "./components/User.vue";
-
+import Quote from "./Components/Quote";
 export default {
   components: {
-    appUser: User
+    appQuote: Quote
   }
 };
 </script>
 
-<style>
-div.component {
-  border: 1px solid black;
-  padding: 30px;
-}
-</style>
+<style></style>
