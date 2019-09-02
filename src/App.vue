236ef0b41e99bdc799f1b5fea0e5baf2da9db18b
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-02 18:21:55 +0400

    feat(source): 153. Using Checkboxes and Saving Data in Arrays

--- src/App.vue
+++ src/App.vue
@@ -56,10 +56,21 @@
         >
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
+              <input
+                type="checkbox"
+                id="sendmail"
+                value="SendMail"
+                v-model="sendMail"
+              />
+              Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail" />
+              <input
+                type="checkbox"
+                id="sendInfomail"
+                value="SendInfoMail"
+                v-model="sendMail"
+              />
               Send Infomail
             </label>
           </div>
@@ -110,7 +121,7 @@
             <p style="white-space: pre">Message: {{ message }}</p>
             <p><strong>Send Mail?</strong></p>
             <ul>
-              <li></li>
+              <li v-for="item in sendMail">{{ item }}</li>
             </ul>
             <p>Gender:</p>
             <p>Priority:</p>
@@ -131,7 +142,8 @@ export default {
         password: "",
         age: 21
       },
-      message: "A new Text"
+      message: "A new Text",
+      sendMail: []
     };
   }
 };
