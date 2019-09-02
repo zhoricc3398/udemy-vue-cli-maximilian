38e4cb505b117187ef8c0aa3132efd3c33635f3c
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-02 16:11:59 +0400

    feat(source): 139. Creating the Application Components

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,14 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
-        <button @click="selectedComponent = 'appAuthor'">Author</button>
-        <button @click="selectedComponent = 'appNew'">New</button>
-        <hr />
-        <p>{{ selectedComponent }}</p>
-        <keep-alive>
-          <component :is="selectedComponent">
-            <p>Default Content</p>
-          </component>
-        </keep-alive>
-        <!--        <app-quote>-->
-        <!--          <h2 slot="title">{{ quoteTitle }}</h2>-->
-        <!--          <p>A wonderful Quote</p>-->
-        <!--        </app-quote>-->
-      </div>
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
-import Author from "./Components/Author";
-import New from "./Components/New";
-import Quote from "./Components/Quote";
-
 export default {
   data() {
     return {
-      quoteTitle: "The Quote",
-      selectedComponent: "appQuote"
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10
     };
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
   }
 };
 </script>
--- src/Components/Quote.vue
+++ src/Components/Quote.vue
@@ -1,34 +1,27 @@
 <template>
-  <div>
-    <div class="title">
-      <slot name="title"></slot>
-      <span style="color: #ccc"><slot name="subtitle">The Subtitle</slot></span>
-    </div>
-    <hr />
-    <div>
-      <slot></slot>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script></script>
 
-<style scoped>
-div {
-  border: 1px solid #ccc;
-  box-shadow: 1px 1px 2px black;
-  padding: 30px;
-  margin: 30px auto;
-  text-align: center;
+<style>
+.panel-body {
+  font-family: "Arizonia", cursive;
+  font-size: 24px;
+  color: #6e6e6e;
 }
 
-h2 {
-  color: red;
+.quote {
+  cursor: pointer;
 }
 
-.title {
-  font-style: italic;
+.quote:hover {
+  background-color: #ffe2e2;
 }
 </style>
