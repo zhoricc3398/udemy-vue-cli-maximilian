3339e9b902e7d3939de66c9000ccac0c86762eb7
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-02 10:36:08 +0400

    feat(source): 107. Using Props for Parent => Child Communication

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-user></app-user>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
+import User from "./components/User.vue";
 
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
-    }
+export default {
+  components: {
+    appUser: User
+  }
+};
 </script>
 
 <style>
-
+div.component {
+  border: 1px solid black;
+  padding: 30px;
+}
 </style>
