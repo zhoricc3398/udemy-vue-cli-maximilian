97413c33da6eb81cb7634b9a1b5b38688b10a09b
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-03 12:26:30 +0400

    feat(source): 171. Using Multiple Modifiers

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,9 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-local-highlight:background.delayed="'red'">Color this, too</p>
+        <p v-local-highlight:background.delayed.blink="'red'">
+          Color this, too
+        </p>
       </div>
     </div>
   </div>
@@ -21,19 +23,37 @@
 <script>
 export default {
   directives: {
-    "local-hightlight": {
+    "local-highlight": {
       bind(el, binding, vnode) {
         var delay = 0;
         if (binding.modifiers["delayed"]) {
           delay = 3000;
         }
-        setTimeout(() => {
-          if (binding.arg == "background") {
-            el.style.backgroundColor = binding.value;
-          } else {
-            el.style.color = binding.value;
-          }
-        }, delay);
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value;
+          let secondColor = "blue";
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
       }
     }
   }
