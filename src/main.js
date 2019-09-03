e767d85967395960e76490a346d85dad60ce2bac
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-03 14:43:09 +0400

    feat(source): 179. An (often-times better) Alternative to Filters: Computed Properties

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-Vue.filter("to-lowercase", function(value) {
+Vue.filter("toLowercase", function(value) {
   return value.toLowerCase();
 });
 
