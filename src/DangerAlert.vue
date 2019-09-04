eb1005ae346fcaa2d278ef0039cb6a3da0bf58f1
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-04 14:17:54 +0400

    feat(source): 212. Adding Animations

--- src/DangerAlert.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="alert alert-danger">This is dangerous!</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped></style>
--- src/SuccessAlert.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="alert alert-success">This is successful!</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped></style>
