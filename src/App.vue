2f3ba4a88e9857d522c0216566850b131168a850
zhoricc3398 <16200126@ibsu.edu.ge>
2019-09-04 15:15:59 +0400

    feat(source): 219. Creating an Application and Setting Up a Server (Firebase)

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,36 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" />
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.user);
+    }
+  }
+};
 </script>
 
 <style></style>
